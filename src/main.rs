59cd7541682de2862bc443eea0c8eff0732667ec
Chen Weiguang <chen.weiguang@gmail.com>
2017-04-04 00:18:38 +0800

    Added most of the string replacement implementations.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate log4rs;
 
 #[macro_use]
 extern crate serde_derive;
+extern crate serde_json;
 extern crate structopt;
 
 #[macro_use]
@@ -15,6 +16,7 @@ extern crate structopt_derive;
 extern crate toml;
 
 use hyper::client::{Client, RedirectPolicy};
+use serde_json::{Map, Value};
 use std::fs::File;
 use std::io::{self, Read, Write};
 use std::process;
@@ -36,6 +38,11 @@ use errors::*;
 #[derive(Serialize, Deserialize, Debug)]
 struct FileConfig {
     update_center_url: String,
+    suppress_front: String,
+    suppress_back: String,
+    connection_check_url_change: String,
+    url_replace_from: String,
+    url_replace_into: String,
 }
 
 #[derive(StructOpt, Debug)]
@@ -46,6 +53,64 @@ struct ArgConfig {
 
     #[structopt(short = "l", long = "log-config", help = "Log configuration file path")]
     log_config_path: String,
+}
+
+// const key names
+const CONNECTION_CHECK_URL_KEY: &'static str = "connectionCheckUrl";
+const CORE_KEY: &'static str = "core";
+const PLUGINS_KEY: &'static str = "plugins";
+const URL_KEY: &'static str = "url";
+
+type MapStrVal = Map<String, Value>;
+
+fn change_connection_check_url<S: Into<String>>(resp_outer_map: &mut MapStrVal, connection_check_url_change: S) -> Result<()> {
+    let connection_check_url = match resp_outer_map.get_mut(CONNECTION_CHECK_URL_KEY) {
+        Some(connection_check_url) => connection_check_url,
+        None => bail!(format!("Unable to find '{}' for changing connection URL", CONNECTION_CHECK_URL_KEY)),
+    };
+
+    let mut connection_check_url = match connection_check_url {
+        &mut Value::String(ref mut connection_check_url) => connection_check_url,
+        c @ _ => bail!(format!("Expected '{}' to contain string value, but found content: {:?}", CONNECTION_CHECK_URL_KEY, c)),
+    };
+
+    *connection_check_url = connection_check_url_change.into();
+    Ok(())
+}
+
+fn replace_url_impl(url_outer: &mut Value, outer_key: &str, url_replace_from: &str, url_replace_into: &str) -> Result<()> {
+    let mut url_outer_map = match url_outer {
+        &mut Value::Object(ref mut url_outer_map) => url_outer_map,
+        c @ _ => bail!(format!("Expected '{}' to be an object, but found content: {:?}", outer_key, c)),
+    };
+
+    let mut url = match url_outer_map.get_mut(URL_KEY) {
+        Some(url) => url,
+        None => bail!(format!("Expected '{}' to be present for '{}'", URL_KEY, CORE_KEY)),
+    };
+
+    let mut url_str = match url {
+        &mut Value::String(ref mut url_str) => url_str,
+        c @ _ => bail!(format!("Expected '{}' to contain string value, but found content: {:?}", URL_KEY, c)),
+    };
+
+    *url_str = url_str.replace(url_replace_from, url_replace_into);
+
+    Ok(())
+}
+
+fn replace_core_url(resp_outer_map: &mut MapStrVal, url_replace_from: &str, url_replace_into: &str) -> Result<()> {
+    let mut core = match resp_outer_map.get_mut(CORE_KEY) {
+        Some(core) => core,
+        None => bail!(format!("Unable to find '{}' for core URL replacement", CORE_KEY)),
+    };
+
+    replace_url_impl(&mut core, CORE_KEY, url_replace_from, url_replace_into)?;
+    Ok(())
+}
+
+fn replace_plugin_urls(resp_outer_map: &mut MapStrVal, url_replace_from: &str, url_replace_into: &str) -> Result<()> {
+    Ok(())
 }
 
 fn run() -> Result<()> {
@@ -83,10 +148,33 @@ fn run() -> Result<()> {
 
     let resp_str = resp_str;
 
+    let trimmed_resp_str = resp_str
+        .trim_left_matches(&config.suppress_front)
+        .trim_right_matches(&config.suppress_back);
+
+    // JSON parsing all the way
+    let mut resp_json: Value = serde_json::from_str(trimmed_resp_str)
+        .chain_err(|| "Unable to parse trimmed JSON string into JSON value.")?;
+
+    // to stop borrowing early
+    {
+        let mut resp_outer_map = match resp_json {
+            Value::Object(ref mut resp_outer_map) => resp_outer_map,
+            c @ _ => bail!(format!("Expected outer most JSON to be of Object type, but found content: {:?}", c)),
+        };
+
+        change_connection_check_url(&mut resp_outer_map, config.connection_check_url_change.to_owned())?;
+        replace_core_url(&mut resp_outer_map, &config.url_replace_from, &config.url_replace_into)?;
+        replace_plugin_urls(&mut resp_outer_map, &config.url_replace_from, &config.url_replace_into)?;
+    }
+
     let mut json_file = File::create("update-center.json")
         .chain_err(|| "Unable to open file for writing")?;
 
-    json_file.write_fmt(format_args!("{}", resp_str))
+    let serialized_json = serde_json::to_string(&resp_json)
+        .chain_err(|| "Unable to convert modified JSON back into string for serialization")?;
+
+    json_file.write_fmt(format_args!("{}", serialized_json))
         .chain_err(|| "Unable to write to file")?;
 
     Ok(())
